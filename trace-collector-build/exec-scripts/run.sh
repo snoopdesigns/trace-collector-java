8a00f067b627c804e8c25ce71e8791e875476435
dimka <snoopdesigns@gmail.com>
2014-05-09 20:28:04 +0400

    Updated linux run script

--- trace-collector-build/exec-scripts/run.sh
+++ trace-collector-build/exec-scripts/run.sh
@@ -1,12 +1,10 @@
 #/bin/sh
-if [ -n "$TERM" ]; then
   cd trace-collector-manager
-  $TERM -e java -jar trace-collector-manager-0.0.1-SNAPSHOT-war-exec.jar &
+  xterm -e java -jar trace-collector-manager-0.0.1-SNAPSHOT-war-exec.jar &
   cd ..
   cd trace-collector-unit
-  $TERM -e java -jar trace-collector-unit-0.0.1-SNAPSHOT-war-exec.jar -httpPort 8181&
+  xterm -e java -jar trace-collector-unit-0.0.1-SNAPSHOT-war-exec.jar -httpPort 8181&
   cd ..
   cd trace-collector-keeper
-  $TERM -e java -jar trace-collector-keeper-emulator-0.0.1-SNAPSHOT-war-exec.jar -httpPort 8282&
-fi
+  xterm -e java -jar trace-collector-keeper-emulator-0.0.1-SNAPSHOT-war-exec.jar -httpPort 8282&
 
