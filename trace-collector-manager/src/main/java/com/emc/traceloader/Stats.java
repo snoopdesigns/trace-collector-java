32a085c68a15bb39de4340ef7ac736b798d42c9d
dimka <snoopdesigns@gmail.com>
2014-05-12 22:30:53 +0400

    Removed redandant Syste.out

--- trace-collector-manager/src/main/java/com/emc/traceloader/Stats.java
+++ trace-collector-manager/src/main/java/com/emc/traceloader/Stats.java
@@ -42,7 +42,6 @@ public class Stats {
         Iterator it = this.hostStatus.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pairs = (Map.Entry)it.next();
-            System.out.println(pairs.getKey() + " = " + pairs.getValue());
             if((HostStatus)(pairs.getKey()) == status) {
                 result ++;
             }
